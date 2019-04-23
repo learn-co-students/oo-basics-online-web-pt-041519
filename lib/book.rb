66e941cb6eb9d764d01c632fc33f8a4d92a45ce8
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-23 13:54:43 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
+class Book
+  attr_accessor :author, :genre, :page_count
+  
+  def initialize(book_title)
+    @title = book_title
+  end 
+  
+  def title
+    @title
+  end 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+  
+end 
 
