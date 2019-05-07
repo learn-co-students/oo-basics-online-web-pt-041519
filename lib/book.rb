a312d613e1c2ea9b0fc37f7bb7e57022210f4f2e
aparkening <aaron.parkening@gmail.com>
2019-05-07 13:34:52 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
+## Create Book Class
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end	
 
+# Test
+# book = Book.new("A Title")
+# puts book
+# book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
-# Make your shoe class here!
+
+## Create Shoe Class
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new" 
+  end
+end	
+
+# Test
+# shoe = Shoe.new("Reebok")
+# puts shoe.brand
+# shoe.condition = "old"
+# shoe.cobble
+# puts shoe.condition
