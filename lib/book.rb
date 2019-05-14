a3f56ef63f52666b916c4d78bda761519b014773
johncban <johncban@gmail.com>
2019-05-14 07:05:45 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
+class Book
 
+  def initialize(title) # setter
+    @title = title
+  end
 
+  def title # getter
+    @title
+  end
+
+  def author=(author) # setter
+    @author = author
+  end
+
+  def author # getter
+    @author
+  end
+
+  def page_count=(n)
+    @page_count = n
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
